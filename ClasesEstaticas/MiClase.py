4c240eea244c00a5b5602a502b6ab174b9eb8090
CesarVelascoAriza <caacesar.ariza@gmail.com>
2024-04-13 22:09:55 +0000

    metodos staticos

--- ClasesEstaticas/MiClase.py
+++ ClasesEstaticas/MiClase.py
@@ -1,5 +1,6 @@
 '''
 variables de clase  de contexto estatico en python
+y varialbes de clase 
 '''
 
 class MiClase:
@@ -7,3 +8,7 @@ class MiClase:
 
     def __init__(self, variable_instancia) -> None:
         self.variable_instancia = variable_instancia
+    
+    @staticmethod
+    def metodo_static():
+        print(MiClase.variable)

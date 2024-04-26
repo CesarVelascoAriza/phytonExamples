8e26f71c3e5177fc7873d53f4746e47ed167ed4d
CesarVelascoAriza <caacesar.ariza@gmail.com>
2024-04-26 03:56:35 +0000

    WebApp

--- Dajango/sap/sap/urls.py
+++ Dajango/sap/sap/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from webApp.views import webApp
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    #path("webApp/", webApp),
+    path("", webApp),
 ]

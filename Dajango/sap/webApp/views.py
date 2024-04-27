daf514bf05a9816d7c281661333e2478654394d1
CesarVelascoAriza <caacesar.ariza@gmail.com>
2024-04-27 03:02:26 +0000

    templates en django

--- Dajango/sap/webApp/views.py
+++ Dajango/sap/webApp/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from personas.models import Persona
 # Create your views here.
 
 def webApp(request):
-    return HttpResponse('Hola mundo desde djago')
+    personas_var_count = Persona.objects.count()
+
+    return render(request, 'index.html',{'count':personas_var_count})
 def despedida(req):
     return HttpResponse("")
